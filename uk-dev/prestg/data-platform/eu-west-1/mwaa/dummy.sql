9a6f51491549664a723a72ade1189fec1660a90f
pankajsnehi <pankaj.snehi@lendable.co.uk>
2022-10-25 13:42:49 +0100

    making a change

--- uk-dev/prestg/data-platform/eu-west-1/mwaa/dummy.sql
+++ uk-dev/prestg/data-platform/eu-west-1/mwaa/dummy.sql
@@ -1,1 +1,1 @@
-writing some sql code for testing and to make popcorn and smoothies and chocolates and milkshakes
+writing some sql code for testing and to make popcorn and smoothies and chocolates and milkshakes and paninis
